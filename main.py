19786b42858e04f76f6d6b5690b533fd46908ffa
pamirshinwari <pamir.forcode@gmail.com>
2025-02-02 13:06:27 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    year = datetime.now().year
+    return render_template("index.html", year=year)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
